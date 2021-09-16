e97521e39051dcc59ad99ce4d1070399015b982c
Khorshed Alam <thekhorshed.alam@gmail.com>
2021-09-16 15:01:03 +0600

    stringify the json body

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,8 @@
 exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       message: "Hello World!",
-    },
+    }),
   };
 };
