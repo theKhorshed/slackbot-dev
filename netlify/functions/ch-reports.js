bfac181cccb012edb2c8e4228373fb1d19e45d4d
Khorshed Alam <thekhorshed.alam@gmail.com>
2021-09-27 16:22:32 +0600

    wip

---
+++ netlify/functions/ch-reports.js
@@ -0,0 +1,47 @@
+const uppercaseModule = require("./utils/uppercase.js");
+const { db, showReport } = require("./utils/firebase-utils.js");
+const { table } = require("table");
+const config = {
+  columns: [
+    { alignment: "left" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+  ],
+  border: {
+    topBody: `─`,
+    topJoin: `┬`,
+    topLeft: `┌`,
+    topRight: `┐`,
+
+    bottomBody: `─`,
+    bottomJoin: `┴`,
+    bottomLeft: `└`,
+    bottomRight: `┘`,
+
+    bodyLeft: `│`,
+    bodyRight: `│`,
+    bodyJoin: `│`,
+
+    joinBody: `─`,
+    joinLeft: `├`,
+    joinRight: `┤`,
+    joinJoin: `┼`,
+  },
+};
+
+exports.handler = async function (event, context) {
+  const data = await showReport();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ table(data, config) }),
+  };
+};
