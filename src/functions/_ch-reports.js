ba19cc51d4e66c9c8d51de5e5a916ddc5e75e7c1
Khorshed Alam <thekhorshed.alam@gmail.com>
2021-09-27 13:31:18 +0600

    wip

---
+++ src/functions/_ch-reports.js
@@ -0,0 +1,47 @@
+import { table } from "table";
+import { showReport } from "./modules/firebase-utils.js";
+const config = {
+  columns: [
+    { alignment: "left" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+    { alignment: "center" },
+  ],
+  border: {
+    topBody: `─`,
+    topJoin: `┬`,
+    topLeft: `┌`,
+    topRight: `┐`,
+
+    bottomBody: `─`,
+    bottomJoin: `┴`,
+    bottomLeft: `└`,
+    bottomRight: `┘`,
+
+    bodyLeft: `│`,
+    bodyRight: `│`,
+    bodyJoin: `│`,
+
+    joinBody: `─`,
+    joinLeft: `├`,
+    joinRight: `┤`,
+    joinJoin: `┼`,
+  },
+};
+
+exports.handler = async function (event, context) {
+  const data = await showReport();
+  data = table(data, config);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data }),
+  };
+};
