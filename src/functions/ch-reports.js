f635a025d049e267d017f827b737977793dae389
Khorshed Alam <thekhorshed.alam@gmail.com>
2021-09-27 13:33:49 +0600

    wip

--- src/functions/ch-reports.js
+++ src/functions/ch-reports.js
@@ -1,44 +1,8 @@
-import { table } from "table";
 import { showReport } from "./modules/firebase-utils.js";
-const config = {
-  columns: [
-    { alignment: "left" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-    { alignment: "center" },
-  ],
-  border: {
-    topBody: `─`,
-    topJoin: `┬`,
-    topLeft: `┌`,
-    topRight: `┐`,
-
-    bottomBody: `─`,
-    bottomJoin: `┴`,
-    bottomLeft: `└`,
-    bottomRight: `┘`,
-
-    bodyLeft: `│`,
-    bodyRight: `│`,
-    bodyJoin: `│`,
-
-    joinBody: `─`,
-    joinLeft: `├`,
-    joinRight: `┤`,
-    joinJoin: `┼`,
-  },
-};
 
 exports.handler = async function (event, context) {
   const data = await showReport();
-  data = table(data, config);
+  //   data = table(data, config);
 
   return {
     statusCode: 200,
